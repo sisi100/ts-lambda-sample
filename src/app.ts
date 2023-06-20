e4ba12218312b11cf62c9a679fc9f6add639c12d
Shun Isii <sisii00.swork@gmail.com>
2023-06-20 09:54:21 +0900

    とりあえずデプロイできたよ

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,14 @@
 import * as cdk from "aws-cdk-lib";
 
 const app = new cdk.App();
-const stack = new cdk.Stack(app, "{{test}}-stack");
+const stack = new cdk.Stack(app, "ts-lambda-sample-stack");
+
+new cdk.aws_lambda_nodejs.NodejsFunction(stack, "function", {
+  entry: "src/assets/index.ts",
+  handler: "handler",
+  architecture: cdk.aws_lambda.Architecture.ARM_64,
+  runtime: cdk.aws_lambda.Runtime.NODEJS_18_X,
+  timeout: cdk.Duration.seconds(10),
+});
+
+app.synth();
---
+++ src/assets/index.ts
@@ -0,0 +1,3 @@
+export const handler = async function (event: any) {
+  console.log("HelloWorld");
+};
